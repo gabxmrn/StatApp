ecb93ac039bde9277334e166c95c5cbdf4124c3d
gabxmrn <149685872+gabxmrn@users.noreply.github.com>
2025-02-08 20:05:15 +0100

    Ajout données françaises

---
+++ code/main.r
@@ -0,0 +1,10 @@
+source("code/utils/import.R")
+
+###### Importation des données ######
+data_fr <- excel_import("code/data.xlsx", "France")
+#! Ajouter une base pour les US
+
+###### Traitement des données ######
+
+#! Croissance du revenue, de la consommation
+#! Differenced short-term interest rate
---
+++ code/utils/import.r
@@ -0,0 +1,12 @@
+excel_import <- function(chemin, feuille) {
+
+  library(readxl)
+
+  df <- read_excel(chemin, sheet = feuille,
+                   col_names = TRUE)
+  df <- as.data.frame(df)
+  rownames(df) <- df[[1]]
+  df <- df[-1]
+
+  return(df)
+}
