e88ca54e24d712068373d6923db752040b6e3e32
cyprienmathe <cyprien.mathe@polytechnique.edu>
2025-03-05 00:26:12 +0100

    commentaires

--- code/calcul_chi.r
+++ code/calcul_chi.r
@@ -1,4 +1,5 @@
 install.packages("dplyr")
+
 source("code/utils/import.R")
 source("code/utils/data_treatment.R")
 source("code/utils/graphs.R")
@@ -9,16 +10,7 @@ source("code/utils/stationarity.R")
 data_fr <- excel_import("code/data.xlsx", "France")
 data_us <- excel_import("code/data.xlsx", "US")
 
-###### Données - Richesse ######
-
-date_debut <- "1997-12-01"
-date_fin <- "2023-12-01"
-
-# richesse_fr <- df_richesse(data_fr, date_debut, date_fin, 1)
-# plot_richesse(richesse_fr, "France")
-
-richesse_us <- df_richesse(data_us, date_debut, date_fin, 1)
-print(head(data_us))
+#print(head(data_us))
 
 #to get lead function
 library(dplyr)
@@ -26,6 +18,8 @@ library(dplyr)
 
 #df des nouvelles variables
 data_us_new_var <- data_us[seq(1, nrow(data_us), by = 4),]
+
+#ajout spread et croissance conso et revenu
 data_us_new_var$spread <- data_us_new_var$taux_lt - data_us_new_var$taux_st
 data_us_new_var <- data_us_new_var %>%
   mutate(income_growth = (revenu - lag(revenu)) / lag(revenu) * 100)
@@ -34,20 +28,23 @@ data_us_new_var <- data_us_new_var %>%
 
 #Obtenir \Delta log(C_t)
 data_us_new_var$delta_log_c <- c(diff(log(data_us$conso[seq(1, nrow(data_us), by = 4)])),NA)
-#Obtenir le lag t-1 : \Delta log(C_(t-1))
-data_us_new_var$lag_delta_log_c <- lead(data_us_new_var$delta_log_c, n = 1)
 
+#Obtenir les lag t-1 et t-2, on les nomme lag1 et lag2 suivi du nom de la variable originale
 data_us_new_var <- data_us_new_var %>%
-  mutate(across(c(chomage, taux_st, conso_growth, spread,confiance,income_growth), ~ lead(.x, n = 2), .names = "lag2_{.col}"))
+  mutate(across(c(chomage, taux_st, conso_growth, spread,confiance,income_growth,delta_log_c), ~ lead(.x, n = 2), .names = "lag2_{.col}"))
 
 data_us_new_var <- data_us_new_var %>%
   mutate(across(c(chomage, taux_st, conso_growth,spread, confiance,income_growth), ~ lead(.x, n = 1), .names = "lag1_{.col}"))
 
-data_us_new_var$lag_delta <- lead(data_us_new_var$conso, n=2)
-print(tail(data_us_new_var))
+#print(tail(data_us_new_var))
 
+#première régression sur les variables instrumentales, pour faire la reg IV de Delta log ct sur son lag t-1
 model_IV <- lm(lag_delta_log_c ~ lag2_chomage + lag2_taux_st + lag2_spread + lag2_conso_growth + lag2_confiance + lag2_income_growth, data = data_us_new_var, na.action = na.omit)
+
+#reconstruction de la prédiction sur les IV
 data_us_new_var$predicted_lag_delta_log_c <- predict(model_IV, newdata = data_us_new_var[,c("lag1_chomage","lag1_conso_growth","lag1_income_growth","lag1_taux_st","lag1_spread","lag1_confiance")])
+
+#régression finale
 model <- lm(delta_log_c ~ predicted_lag_delta_log_c + cpi, data = data_us_new_var, na.action = na.omit)
 chi <- coef(model)["predicted_lag_delta_log_c"]
 
