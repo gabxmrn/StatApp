bc5a8b048a4ab69e9fd2ef2f9e323f4ea0eba948
cyprienmathe <cyprien.mathe@polytechnique.edu>
2025-03-05 01:00:54 +0100

    corrections sur calcul_chi.r chi_us = 1

--- code/calcul_chi.r
+++ code/calcul_chi.r
@@ -20,7 +20,7 @@ library(dplyr)
 data_us_new_var <- data_us[seq(1, nrow(data_us), by = 4),]
 
 #ajout spread et croissance conso et revenu
-data_us_new_var$spread <- data_us_new_var$taux_lt - data_us_new_var$taux_st
+data_us_new_var$spread <- data_us_new_var$taux_lt - data_us_new_var$taux_ct
 data_us_new_var <- data_us_new_var %>%
   mutate(income_growth = (revenu - lag(revenu)) / lag(revenu) * 100)
 data_us_new_var <- data_us_new_var %>%
@@ -31,21 +31,25 @@ data_us_new_var$delta_log_c <- c(diff(log(data_us$conso[seq(1, nrow(data_us), by
 
 #Obtenir les lag t-1 et t-2, on les nomme lag1 et lag2 suivi du nom de la variable originale
 data_us_new_var <- data_us_new_var %>%
-  mutate(across(c(chomage, taux_st, conso_growth, spread,confiance,income_growth,delta_log_c), ~ lead(.x, n = 2), .names = "lag2_{.col}"))
+  mutate(across(c(chomage, taux_ct, conso_growth, spread,confiance,income_growth), ~ lead(.x, n = 2), .names = "lag2_{.col}"))
 
 data_us_new_var <- data_us_new_var %>%
-  mutate(across(c(chomage, taux_st, conso_growth,spread, confiance,income_growth), ~ lead(.x, n = 1), .names = "lag1_{.col}"))
+  mutate(across(c(chomage, taux_ct, conso_growth,spread , confiance,income_growth,delta_log_c), ~ lead(.x, n = 1), .names = "lag1_{.col}"))
 
 #print(tail(data_us_new_var))
 
 #première régression sur les variables instrumentales, pour faire la reg IV de Delta log ct sur son lag t-1
-model_IV <- lm(lag_delta_log_c ~ lag2_chomage + lag2_taux_st + lag2_spread + lag2_conso_growth + lag2_confiance + lag2_income_growth, data = data_us_new_var, na.action = na.omit)
+model_IV <- lm(lag1_delta_log_c ~ lag2_chomage + lag2_taux_ct + lag2_spread + lag2_conso_growth + lag2_confiance + lag2_income_growth, data = data_us_new_var, na.action = na.omit)
 
 #reconstruction de la prédiction sur les IV
-data_us_new_var$predicted_lag_delta_log_c <- predict(model_IV, newdata = data_us_new_var[,c("lag1_chomage","lag1_conso_growth","lag1_income_growth","lag1_taux_st","lag1_spread","lag1_confiance")])
+new_data <- data_us_new_var %>%
+  select(lag1_chomage ,lag1_taux_ct,lag1_spread,lag1_conso_growth, lag1_confiance, lag1_income_growth) %>%  # Sélectionner les colonnes
+  rename(lag2_chomage = lag1_chomage, lag2_taux_ct = lag1_taux_ct, lag2_spread = lag1_spread, lag2_conso_growth = lag1_conso_growth, lag2_confiance = lag1_confiance, lag2_income_growth = lag1_income_growth)
+
+data_us_new_var$predicted_lag1_delta_log_c <- predict(model_IV, newdata = new_data)
 
 #régression finale
-model <- lm(delta_log_c ~ predicted_lag_delta_log_c + cpi, data = data_us_new_var, na.action = na.omit)
-chi <- coef(model)["predicted_lag_delta_log_c"]
+model <- lm(delta_log_c ~ predicted_lag1_delta_log_c + cpi, data = data_us_new_var, na.action = na.omit)
+chi <- coef(model)["predicted_lag1_delta_log_c"]
 
 print(chi)
