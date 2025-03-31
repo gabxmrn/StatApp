d188eea8703bbca9e0f0b2e86bc8971c3c1ffe8f
gabxmrn <149685872+gabxmrn@users.noreply.github.com>
2025-03-31 14:02:54 +0200

    Ajout outputs annexes pour la table du chi (type CLR moreira)

--- code/models/chi.r
+++ code/models/chi.r
@@ -1,75 +1,87 @@
 
-#data_us_new_var n'est pas utilisé que pour les US
+#data_new n'est pas utilisé que pour les US
 #c'est une ancienne notation
 
 chi <- function(df, date_debut, date_fin, freq) {
 
-  #to get lag function
+  # Librairies
   library(dplyr)
+  library(ivmodel)
 
   #df des nouvelles variables
   df <- df[rownames(df) >= date_debut & rownames(df) <= date_fin, ]
-  data_us_new_var <- df[seq(1, nrow(df), by = freq), ]
+  data_new <- df[seq(1, nrow(df), by = freq), ]
 
   #ajout spread et croissance conso et revenu, (correction par cpi nécessaire ? faite)
-  data_us_new_var <- data_us_new_var %>%
+  data_new <- data_new %>%
     mutate(conso = conso/(population),
     revenu = revenu/(population),
       income_growth = (revenu - lag(revenu)) / lag(revenu) * 100,
     conso_growth = (conso - lag(conso)) / lag(conso) * 100)
 
   #Donne le "differenced" taux_ct
-  data_us_new_var <- data_us_new_var %>%
+  data_new <- data_new %>%
     mutate(diff_taux_ct = (taux_ct - lag(taux_ct)))
 
 
   #Obtenir \Delta log(C_t), représente la variation de C_t
-  data_us_new_var$delta_log_c <- c(NA, diff(log(data_us_new_var$conso)))
+  data_new$delta_log_c <- c(NA, diff(log(data_new$conso)))
 
 
   #Obtenir les lag t-1 et t-2, on les nomme lag1 et lag2 suivi du nom de la variable originale
-  data_us_new_var <- data_us_new_var %>%
+  data_new <- data_new %>%
     mutate(across(c(chomage, diff_taux_ct, conso_growth, spread,confiance,income_growth), ~ lag(.x, n = 2), .names = "lag2_{.col}"))
 
-  data_us_new_var <- data_us_new_var %>%
+  data_new <- data_new %>%
     mutate(across(c(chomage, diff_taux_ct, conso_growth,spread , confiance,income_growth,delta_log_c), ~ lag(.x, n = 1), .names = "lag1_{.col}"))
 
   #première régression sur les variables instrumentales, pour faire la reg IV de Delta log ct sur son lag t-1
-  model_IV <- lm(lag1_delta_log_c ~ lag2_chomage + lag2_diff_taux_ct + lag2_spread + lag2_conso_growth + lag2_confiance + lag2_income_growth, data = data_us_new_var, na.action = na.omit)
+  model_IV <- lm(lag1_delta_log_c ~ lag2_chomage + lag2_diff_taux_ct + lag2_spread + lag2_conso_growth + lag2_confiance + lag2_income_growth, data = data_new, na.action = na.omit)
 
   #reconstruction de la prédiction sur les IV
-  #new_data <- data_us_new_var %>%
+  #new_data <- data_new %>%
   #  select(lag1_chomage ,lag1_diff_taux_ct,lag1_spread,lag1_conso_growth, lag1_confiance, lag1_income_growth) %>%  # Sélectionner les colonnes
   #  rename(lag2_chomage = lag1_chomage, lag2_diff_taux_ct = lag1_diff_taux_ct, lag2_spread = lag1_spread, lag2_conso_growth = lag1_conso_growth, lag2_confiance = lag1_confiance, lag2_income_growth = lag1_income_growth)
 
   #construction de l'argument pour faire la prédiction du régressseur dans la 2eme partie de l'IV reg
-  new_data <- data_us_new_var %>%
+  new_data <- data_new %>%
     select(lag2_chomage ,lag2_diff_taux_ct,lag2_spread,lag2_conso_growth, lag2_confiance, lag2_income_growth) # Sélectionner les colonnes
 
-  data_us_new_var$predicted_lag1_delta_log_c <- predict(model_IV, newdata = new_data)
+  data_new$predicted_lag1_delta_log_c <- predict(model_IV, newdata = new_data)
 
   #régression finale
-  model <- lm(delta_log_c ~ predicted_lag1_delta_log_c, data = data_us_new_var, na.action = na.omit)
+  model <- lm(delta_log_c ~ predicted_lag1_delta_log_c, data = data_new, na.action = na.omit)
 
-  #Essai avec une IV reg intégrée (les deux donnent les memes chi sur fr et us)
-  library(AER)  # Load the package for ivreg
+  # Utilisation d'un package, pour tous les outputs annexes
+  y <- data_new$delta_log_c
+  x <- data_new$lag1_delta_log_c
+  z <- cbind(data_new$lag2_chomage,
+             data_new$lag2_diff_taux_ct,
+             data_new$lag2_spread,
+             data_new$lag2_conso_growth,
+             data_new$lag2_confiance,
+             data_new$lag2_income_growth)
 
-  iv_model <- ivreg(delta_log_c ~ lag1_delta_log_c | lag2_chomage + lag2_diff_taux_ct + lag2_spread + lag2_conso_growth + lag2_confiance + lag2_income_growth, data = data_us_new_var, na.action = na.omit)
+  # Régression instrumentale
+  iv_model <- ivmodel(Y = y, D = x, Z = z)
 
-  chi <- coef(iv_model)["lag1_delta_log_c"]
+  # Coefficients
+  chi <- iv_model$kClass$point.est[2]
+  std <- iv_model$kClass$std.err[2]
+  pval <- iv_model$kClass$p.value[2]
 
-  coef_lag1 <- summary(iv_model)$coefficients["lag1_delta_log_c", 1]  # Coefficient
-  std_lag1 <- summary(iv_model)$coefficients["lag1_delta_log_c", 2]   # Écart-type
-  p_value_lag1 <- summary(iv_model)$coefficients["lag1_delta_log_c", 4]   # p-value
-  #print(summary(iv_model))
+  # Moreira CLR (2003)
+  clr <- iv_model$CLR$ci
+  rob_pval <- iv_model$CLR$p.value
 
   #Pour le calcul ultérieur de la PMC (résidu de l'ivreg)
   residuals <- c(NA,NA,NA,as.vector(residuals(model)))
 
-  result <- list(chi = coef_lag1, std_chi = std_lag1, p_value = p_value_lag1, residuals = residuals)
+  result <- list(model = iv_model,
+                 chi = chi, std_chi = std, p_value = pval, residuals = residuals,
+                 clr = clr, robpval = rob_pval)
 
   return(result)
-
 }
 
 #Fonction pour le tracé de chi en fonction du temps
